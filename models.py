77381ddb34e233dd4b2e2c88027c78b6a2a0f700
pabloin1 <pablocesaraltuzar04@gmail.com>
2024-07-22 23:06:03 -0600

    agregar cors

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ db = SQLAlchemy()
 
 class Ventas(db.Model):
     __tablename__ = 'Ventas'  # Asegúrate de que esto coincida con el nombre en tu base de datos
-    id = db.Column(db.String, primary_key=True)
+    id = db.Column(db.String, primary_key=True) #dasdasdadada
     precio_Fn = db.Column(db.Float, nullable=False)
     id_mesa = db.Column(db.String, nullable=False)
     estado = db.Column(db.Boolean, default=True)
